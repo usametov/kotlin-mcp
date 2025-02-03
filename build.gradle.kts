9a6a9c1c86a5f0edd76b5d4e405537b83c26edf2
Alan Samet <kevin.krebson@gmail.com>
2025-02-03 14:39:19 -0500

    drop shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.20"
     application
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    //id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 allprojects {
